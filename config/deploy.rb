35ed74f6704f84f0ef1e808ab8a073cb2d225175
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2017-09-16 08:32:03 +0900

    init unicorn2

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock '3.6.0'
 
 # デプロイするアプリケーション名
-set :application, 'achieve'
+set :application, 'ganmenbook'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
 set :repo_url, 'https://github.com/shiraryu/ganmenbook'
@@ -10,7 +10,7 @@ set :repo_url, 'https://github.com/shiraryu/ganmenbook'
 set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
-set :deploy_to, '/var/www/achieve'
+set :deploy_to, '/var/www/ganmenbook'
 
 # シンボリックリンクをはるフォルダ・ファイル
 set :linked_files, %w{.env config/secrets.yml}
