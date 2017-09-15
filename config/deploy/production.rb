335818e153a33d85fea72372f77c1027375d3897
白石隆二 <shiraishiryuji@shiraishiryuujinoMacBook-Air.local>
2017-09-16 04:00:47 +0900

    init unicorn2

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '54.65.207.254', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/shiraishiryuji/.ssh'
+server '52.192.184.3', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/shiraishiryuji/.ssh/id_rsa'
